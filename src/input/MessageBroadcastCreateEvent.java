be7e186091863a778e00217dfac8b2b456c490f0
Maximilian Geitner <maximilian.geitner@outlook.de>
2021-12-01 18:01:07 +0100

    added first classes for spreading model

---
+++ src/input/MessageBroadcastCreateEvent.java
@@ -0,0 +1,57 @@
+package input;
+
+import core.DTNHost;
+import core.Message;
+import core.World;
+
+public class MessageBroadcastCreateEvent extends ExternalEvent {
+    /** address of the node the message is from */
+    protected int fromAddr;
+    /** identifier of the message */
+    protected String id;
+
+    private int size;
+    private int responseSize;
+    private int[] receivers;
+    /**
+     * Creates a message creation event with a optional response request
+     * @param from The creator of the message
+     * @param receivers Where the message is destined to
+     * @param id ID of the message
+     * @param size Size of the message
+     * @param responseSize Size of the requested response message or 0 if
+     * no response is requested
+     * @param time Time, when the message is created
+     */
+    public MessageBroadcastCreateEvent(int from, int[] receivers, String id, int size,
+                                       int responseSize, double time) {
+        super(time);
+        this.fromAddr = from;
+        this.id = id;
+        this.receivers = receivers;
+        this.size = size;
+        this.responseSize = responseSize;
+    }
+
+    /**
+     * Creates the message this event represents.
+     */
+    @Override
+    public void processEvent(World world) {
+        for (int toAddr : receivers) {
+            DTNHost to = world.getNodeByAddress(toAddr);
+            DTNHost from = world.getNodeByAddress(this.fromAddr);
+
+            Message m = new Message(from, to, this.id, this.size);
+            m.setResponseSize(this.responseSize);
+            from.createNewMessage(m);
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " [" + fromAddr + "->" + receivers.length + " hosts] " +
+                "size:" + size + " CREATE";
+    }
+}
---
+++ src/report/InfectionReport.java
@@ -0,0 +1,118 @@
+package report;
+
+import core.DTNHost;
+import core.Message;
+import core.MessageListener;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+public class InfectionReport
+        extends Report
+        implements MessageListener {
+
+    private HashMap<Integer, InfectionData> infectionTracker;
+    private Random random;
+
+    public InfectionReport(){
+       init();
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+        random = new Random();
+        infectionTracker = new HashMap<Integer, InfectionData>();
+    }
+
+    @Override
+    public void newMessage(Message m) {
+
+    }
+
+    @Override
+    public void messageTransferStarted(Message m, DTNHost from, DTNHost to) {
+
+    }
+
+    @Override
+    public void messageDeleted(Message m, DTNHost where, boolean dropped) {
+
+    }
+
+    @Override
+    public void messageTransferAborted(Message m, DTNHost from, DTNHost to) {
+
+    }
+
+    @Override
+    public void messageTransferred(Message m, DTNHost from, DTNHost to, boolean firstDelivery) {
+        int toAddr = from.getAddress();
+        boolean isFirstInfection = false;
+        if(infectionTracker.containsKey(toAddr)){
+            isFirstInfection = infectionTracker.get(toAddr).addParticles(m.getSize());
+        }else{
+            // 15 * 1/(60 * 15)
+            InfectionData data = new InfectionData(random, 1.0/(15 * 4));
+            isFirstInfection = data.addParticles(m.getSize());
+            infectionTracker.put(toAddr, data);
+        }
+        if(isFirstInfection){
+            write(getSimTime() + ", " + toAddr);
+        }
+    }
+
+    @Override
+    public void done() {
+        //write summary
+        write("-------");
+        //HashMap.SimpleEntry<Integer, InfectionData>
+        for(Map.Entry<Integer, InfectionData> entry:infectionTracker.entrySet()){
+            InfectionData data = entry.getValue();
+            write(entry.getKey()  + ",");
+        }
+
+        super.done();
+    }
+
+    public class InfectionData {
+
+        private boolean isInfected;
+        private int receivedParticles;
+        private double probInfection;
+        private Random r;
+
+        public InfectionData(Random r, double probInfection){
+            isInfected = false;
+            receivedParticles = 0;
+            this.r = r;
+            this.probInfection = probInfection;
+            if(probInfection < 0 || probInfection > 1) throw new IllegalArgumentException("Probability must be between 0 and 1");
+        }
+
+        /**
+         *
+         * @param amount    size of message, indicates virus load in message (higher from unvaccinated or non-mask sender, lower from vaccinated person)
+         * @return did an infection occur?
+         */
+        public boolean addParticles(int amount){
+            if(isInfected) return false;
+            if(amount < 0) throw new IllegalArgumentException("Virus load can't be negative");
+            int currentLevel = receivedParticles / 1000;
+            receivedParticles += amount;
+            int amountRolls = (receivedParticles/1000) - currentLevel;
+            for(int i = 0; i < amountRolls; i++){
+                if (r.nextDouble() < probInfection){
+                    isInfected = true;
+                    return true;
+                }else{
+                    continue;
+                }
+            }
+            return false;
+        }
+    }
+
+}
