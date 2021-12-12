d978f54de09f9048f261319e120104ded8843949
Sven Andabaka <sven.andabaka@tum.de>
2021-12-12 13:04:11 +0100

    refactor InfectionReport to use mobility model to get state

--- src/report/InfectionReport.java
+++ src/report/InfectionReport.java
@@ -1,9 +1,9 @@
 package report;
 
-import core.Coord;
 import core.DTNHost;
 import core.Message;
 import core.MessageListener;
+import movement.StatefulRwp;
 import movement.state.states.*;
 
 import java.util.*;
@@ -199,6 +199,18 @@ public class InfectionReport
 
     @Override
     public void messageTransferred(Message m, DTNHost from, DTNHost to, boolean firstDelivery) {
+
+        String fromLabelLocation = "Other";
+        String toLabelLocation = "Other";
+        try {
+            StatefulRwp movModel = (StatefulRwp) from.movement;
+            fromLabelLocation = movModel.lastState.getStateName();
+            movModel = (StatefulRwp) to.movement;
+            toLabelLocation = movModel.lastState.getStateName();
+        } catch (Exception e) {
+            // Do nothing
+        }
+
         if(m.getFrom().getAddress() != from.getAddress()) return; //only count messages that are from the first sender
         // infected group starts with group ID "I", infected groups can't get infected
         if(to.toString().startsWith("I")) return;
@@ -206,7 +218,7 @@ public class InfectionReport
         if(!to.isMovementActive()) return;
         //if(m.getFrom().getAddress() == to.getAddress()) return;  // message travelled in a loop
         // people in the queue can't get infected
-        if(isInside(new QueueState().getPolygon(), to.getLocation())) return;
+        if (toLabelLocation == new QueueState().getStateName()) return;
         int toAddr = to.getAddress();
 
 
@@ -232,18 +244,18 @@ public class InfectionReport
         double factorSenderLocation = factorLocation[INDEX_INDOOR];
         boolean isHighRiskArea = false;
         for(NodeState node: statesHighRiskPartyAreas) {
-            if (isInside(node.getPolygon(), from.getLocation())) {
+            if (node.getStateName() == fromLabelLocation) {
                 isHighRiskArea = true;
                 break;
             }
         }
         if(isHighRiskArea){
             factorSenderLocation = factorLocation[INDEX_HIGH_RISK_PARTY_AREA];
-        }else if(isInside(stateShishaBar.getPolygon(), from.getLocation())){
+        }else if(stateShishaBar.getStateName() == fromLabelLocation){
             factorSenderLocation = factorLocation[INDEX_SHISHA_BAR]; // check shisha bar
         }else {
             for(NodeState node : statesOutside){
-                if(isInside(node.getPolygon(), from.getLocation())){
+                if(node.getStateName() == fromLabelLocation){
                     factorSenderLocation = factorLocation[INDEX_OUTDOOR];
                     break;
                 }
@@ -292,7 +304,7 @@ public class InfectionReport
         if(isFirstInfection){
             String labelLocation = "Other";
             for (NodeState state : states) {
-                if (isInside(state.getPolygon(), to.getLocation())) {
+                if (state.getStateName() == toLabelLocation) {
                     labelLocation = state.getStateName();
                     break;
                 }
@@ -349,81 +361,4 @@ public class InfectionReport
             return false;
         }
     }
-
-
-    //--------------- methods from ProhibitedPolygonRwp, methods needed to map point to a certain area on the map -------------
-
-    private static boolean isInside(
-            final List <Coord> polygon,
-            final Coord point ) {
-        final int count = countIntersectedEdges( polygon, point,
-                new Coord( -10,0 ) );
-        return ( ( count % 2 ) != 0 );
-    }
-
-    private static int countIntersectedEdges(
-            final List<Coord> polygon,
-            final Coord start,
-            final Coord end ) {
-        int count = 0;
-        for ( int i = 0; i < polygon.size() - 1; i++ ) {
-            final Coord polyP1 = polygon.get( i );
-            final Coord polyP2 = polygon.get( i + 1 );
-
-            final Coord intersection = intersection( start, end, polyP1, polyP2 );
-            if ( intersection == null ) continue;
-
-            if ( isOnSegment( polyP1, polyP2, intersection )
-                    && isOnSegment( start, end, intersection ) ) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    private static boolean isOnSegment(
-            final Coord L0,
-            final Coord L1,
-            final Coord point ) {
-        final double crossProduct
-                = ( point.getY() - L0.getY() ) * ( L1.getX() - L0.getX() )
-                - ( point.getX() - L0.getX() ) * ( L1.getY() - L0.getY() );
-        if ( Math.abs( crossProduct ) > 0.0000001 ) return false;
-
-        final double dotProduct
-                = ( point.getX() - L0.getX() ) * ( L1.getX() - L0.getX() )
-                + ( point.getY() - L0.getY() ) * ( L1.getY() - L0.getY() );
-        if ( dotProduct < 0 ) return false;
-
-        final double squaredLength
-                = ( L1.getX() - L0.getX() ) * ( L1.getX() - L0.getX() )
-                + (L1.getY() - L0.getY() ) * (L1.getY() - L0.getY() );
-        return !(dotProduct > squaredLength);
-    }
-
-    private static Coord intersection(
-            final Coord L0_p0,
-            final Coord L0_p1,
-            final Coord L1_p0,
-            final Coord L1_p1 ) {
-        final double[] p0 = getParams( L0_p0, L0_p1 );
-        final double[] p1 = getParams( L1_p0, L1_p1 );
-        final double D = p0[ 1 ] * p1[ 0 ] - p0[ 0 ] * p1[ 1 ];
-        if ( D == 0.0 ) return null;
-
-        final double x = ( p0[ 2 ] * p1[ 1 ] - p0[ 1 ] * p1[ 2 ] ) / D;
-        final double y = ( p0[ 2 ] * p1[ 0 ] - p0[ 0 ] * p1[ 2 ] ) / D;
-
-        return new Coord( x, y );
-    }
-
-    private static double[] getParams(
-            final Coord c0,
-            final Coord c1 ) {
-        final double A = c0.getY() - c1.getY();
-        final double B = c0.getX() - c1.getX();
-        final double C = c0.getX() * c1.getY() - c0.getY() * c1.getX();
-        return new double[] { A, B, C };
-    }
-
 }
